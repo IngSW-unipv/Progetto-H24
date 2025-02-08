3cb1ad25ea096af25f226839bf4c03c4186b227a
moustafa elsamanoudy <146879859+moustafa-prog@users.noreply.github.com>
2025-02-08 23:18:46 +0100

    Add files via upload

---
+++ src/DataBase/RegistrazioneDAO.java
@@ -0,0 +1,12 @@
+package DataBase;
+
+import java.sql.SQLException;
+
+import Model.User;
+
+public interface RegistrazioneDAO {
+
+ public static void registraUtente(User user) throws SQLException {
+ }
+	
+}
